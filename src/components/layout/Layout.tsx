2c865f50b33d363db316664e5d293954f212724b
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2023-04-21 13:37:58 +0300

    add meta

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,6 +9,7 @@ const Layout: FC<PropsWithChildren<unknown>> = ({ children }) => {
             <Head>
                 <title>Spa Alex | массажный салон</title>
                 <meta name="description" content="spa-alex.ru массажный кабинет на Филях, ТЦ Филион. массаж лица, лимфодренажный, баночный, антицеллюлитный"></meta>
+                <link rel="apple-touch-icon" href="favicon.ico" />
             </Head>
             <Header />
             <main>

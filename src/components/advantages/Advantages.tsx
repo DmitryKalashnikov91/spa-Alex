7c7839b8eb6e26c0ccc9d7d70b28abb9399b1aa1
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2024-04-07 10:37:00 +0300

    deleted animation

--- src/components/advantages/Advantages.tsx
+++ src/components/advantages/Advantages.tsx
@@ -1,4 +1,3 @@
-import { animated, useInView } from '@react-spring/web'
 import Link from 'next/link'
 import { BiSolidSpa } from 'react-icons/bi'
 import { FaUserNinja } from 'react-icons/fa'
@@ -7,59 +6,34 @@ import { TbMessageCircleStar } from 'react-icons/tb'
 import styles from './Advatages.module.scss'
 
 const Advantages = () => {
-	const [ref, inView] = useInView(() => ({
-		from: {
-			opacity: 0,
-		},
-		to: {
-			opacity: 1,
-		},
-	}))
-	const [ref1, inView1] = useInView(() => ({
-		from: {
-			opacity: 0,
-		},
-		to: {
-			opacity: 1,
-		},
-	}))
-
 	return (
 		<section className={styles.Advantages}>
-			<animated.div ref={ref} style={inView} className={styles.Advantages_card}>
+			<div className={styles.Advantages_card}>
 				{
 					<Link href={'#about'}>
 						<BiSolidSpa size={50} />
 						Виды услуг
 					</Link>
 				}
-			</animated.div>
-			<animated.div
-				ref={ref1}
-				style={inView1}
-				className={styles.Advantages_card}
-			>
+			</div>
+			<div className={styles.Advantages_card}>
 				<Link href={'/masseures'}>
 					<FaUserNinja size={50} />
 					Массажисты
 				</Link>
-			</animated.div>
-			<animated.div ref={ref} style={inView} className={styles.Advantages_card}>
+			</div>
+			<div className={styles.Advantages_card}>
 				<Link href={'/gallery'}>
 					<ImImage size={50} />
 					Галерея
 				</Link>
-			</animated.div>
-			<animated.div
-				ref={ref1}
-				style={inView1}
-				className={styles.Advantages_card}
-			>
+			</div>
+			<div className={styles.Advantages_card}>
 				<Link href={'#reviews'}>
 					<TbMessageCircleStar size={50} />
 					Отзывы
 				</Link>
-			</animated.div>
+			</div>
 		</section>
 	)
 }
